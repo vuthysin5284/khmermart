3acedec291b319e798297f2dc65595877bc233ab
vuthysin5284 <vuthy.sin5284@gmail.com>
2017-05-04 00:55:51 +0700

    change menus

--- application/modules/Menu/Views/index.php
+++ application/modules/Menu/Views/index.php
@@ -1,71 +1,68 @@
 <ul class="menu accordion-menu">
     <li class="nav-heading"><span>Navigation</span></li>
 
-    <li><a href="index.html"><span class="menu-icon icon-speedometer"></span><p>Dashboard</p></a></li>
+
+    <li class="droplink"><a href="#"><span class="menu-icon icon-grid"></span><p>FOODS</p><span class="arrow"></span></a>
+        <ul class="sub-menu">
+            <li><a href="<?php echo base_url(); ?>customer">Photographer List</a></li>
+        </ul>
+    </li>
+
+
+    <li class="droplink"><a href="#"><span class="menu-icon icon-grid"></span><p>SHOP</p><span class="arrow"></span></a>
+        <ul class="sub-menu">
+            <li><a href="<?php echo base_url(); ?>customer">Photographer List</a></li>
+        </ul>
+    </li>
+
+    <li class="droplink"><a href="#"><span class="menu-icon icon-bar-chart"></span><p>Sections</p><span class="arrow"></span></a>
+        <ul class="sub-menu">
+            <li><a href="<?php echo base_url(); ?>customer">Coordination</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Schedule</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Budget estimator</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Detailed budget</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Guest list</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Invitations</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Seating chart</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Gifts</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Venue</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Hotel</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Attire</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Hair & makeup</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Flowers</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Cake</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Caterer</a></li>
+        </ul>
+    </li>
 
     <li class="nav-heading"><span>Features</span></li>
 
-    <li class="droplink"><a href="#"><span class="menu-icon icon-briefcase"></span><p>UI Kits</p><span class="arrow"></span></a>
+
+
+    <li class="droplink"><a href="#"><span class="menu-icon icon-grid"></span><p>Photographer</p><span class="arrow"></span></a>
         <ul class="sub-menu">
-            <li><a href="ui-alerts.html">Alerts</a></li>
-            <li><a href="ui-buttons.html">Buttons</a></li>
-            <li><a href="ui-icons.html">Icons</a></li>
-            <li><a href="ui-typography.html">Typography</a></li>
-            <li><a href="ui-notifications.html">Notifications</a></li>
-            <li><a href="ui-grid.html">Grid</a></li>
-            <li><a href="ui-tabs-accordions.html">Tabs &amp; Accordions</a></li>
-            <li><a href="ui-modals.html">Modals</a></li>
-            <li><a href="ui-panels.html">Panels</a></li>
-            <li><a href="ui-progress.html">Progress Bars</a></li>
-            <li><a href="ui-sliders.html">Sliders</a></li>
-            <li><a href="ui-nestable.html">Nestable</a></li>
-            <li><a href="ui-tree-view.html">Tree View</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Photographer List</a></li>
         </ul>
     </li>
-    <li class="droplink active open"><a href="#"><span class="menu-icon icon-layers"></span><p>Layouts</p><span class="arrow"></span></a>
+    <li class="droplink"><a href="#"><span class="menu-icon icon-grid"></span><p>Videographer</p><span class="arrow"></span></a>
         <ul class="sub-menu">
-            <li class="active"><a href="layout-blank.html">Blank Page</a></li>
-            <li><a href="layout-fixed-sidebar.html">Fixed Menu</a></li>
-            <li><a href="layout-static-header.html">Static Header</a></li>
-            <li><a href="layout-collapsed-sidebar.html">Collapsed Sidebar</a></li>
-            <li><a href="layout-large-menu.html">Large Menu</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Videographer List</a></li>
         </ul>
     </li>
-    <li class="droplink"><a href="#"><span class="menu-icon icon-grid"></span><p>Tables</p><span class="arrow"></span></a>
+    <li class="droplink"><a href="#"><span class="menu-icon icon-grid"></span><p>Entertainment</p><span class="arrow"></span></a>
         <ul class="sub-menu">
-            <li><a href="table-static.html">Static Tables</a></li>
-            <li><a href="table-responsive.html">Responsive Tables</a></li>
-            <li><a href="table-data.html">Data Tables</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Entertainment List</a></li>
         </ul>
     </li>
-    <li class="droplink"><a href="#"><span class="menu-icon icon-note"></span><p>Forms</p><span class="arrow"></span></a>
+    <li class="droplink"><a href="#"><span class="menu-icon icon-note"></span><p>Music</p><span class="arrow"></span></a>
         <ul class="sub-menu">
-            <li><a href="form-elements.html">Form Elements</a></li>
-            <li><a href="form-wizard.html">Form Wizard</a></li>
-            <li><a href="form-upload.html">File Upload</a></li>
-            <li><a href="form-image-crop.html">Image Crop</a></li>
-            <li><a href="form-image-zoom.html">Image Zoom</a></li>
-            <li><a href="form-select2.html">Select2</a></li>
-            <li><a href="form-x-editable.html">X-editable</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">Music List</a></li>
         </ul>
     </li>
-    <li class="droplink"><a href="#"><span class="menu-icon icon-bar-chart"></span><p>Charts</p><span class="arrow"></span></a>
+
+    <li class="droplink"><a href="#"><span class="menu-icon icon-user"></span><p>To-do</p><span class="arrow"></span></a>
         <ul class="sub-menu">
-            <li><a href="charts-sparkline.html">Sparkline</a></li>
-            <li><a href="charts-rickshaw.html">Rickshaw</a></li>
-            <li><a href="charts-morris.html">Morris</a></li>
-            <li><a href="charts-flotchart.html">Flotchart</a></li>
-            <li><a href="charts-chartjs.html">Chart.js</a></li>
-        </ul>
-    </li>
-    <li class="droplink"><a href="#"><span class="menu-icon icon-user"></span><p>Login</p><span class="arrow"></span></a>
-        <ul class="sub-menu">
-            <li><a href="login.html">Login Form</a></li>
-            <li><a href="login-alt.html">Login Alt</a></li>
-            <li><a href="register.html">Register Form</a></li>
-            <li><a href="register-alt.html">Register Alt</a></li>
-            <li><a href="forgot.html">Forgot Password</a></li>
-            <li><a href="lock-screen.html">Lock Screen</a></li>
+            <li><a href="<?php echo base_url(); ?>customer">To-do list</a></li>
         </ul>
     </li>
     <li class="droplink"><a href="#"><span class="menu-icon icon-pointer"></span><p>Maps</p><span class="arrow"></span></a>
@@ -78,18 +75,6 @@
         <a href="#"><span class="menu-icon icon-present"></span><p>Master data</p><span class="arrow"></span></a>
         <ul class="sub-menu">
             <li><a href="<?php echo base_url(); ?>customer">Store List</a></li>
-            <li><a href="500.html">500 Page</a></li>
-            <li><a href="invoice.html">Invoice</a></li>
-            <li><a href="calendar.html">Calendar</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="todo.html">Todo</a></li>
-            <li><a href="pricing-tables.html">Pricing Tables</a></li>
-            <li><a href="shop.html">Shop</a></li>
-            <li><a href="gallery.html">Gallery</a></li>
-            <li><a href="timeline.html">Timeline</a></li>
-            <li><a href="search.html">Search Results</a></li>
-            <li><a href="coming-soon.html">Coming Soon</a></li>
-            <li><a href="contact.html">Contact</a></li>
         </ul>
     </li>
 </ul>
