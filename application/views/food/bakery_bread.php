da6e77ce2a688bb0fad1f5800553f132cf3d805f
vuthysin5284 <vuthy.sin5284@gmail.com>
2017-05-27 17:49:55 +0700

    modified code

--- application/views/food/bakery_bread.php
+++ application/views/food/bakery_bread.php
@@ -7,10 +7,26 @@
 
 <div class="row">
     <div class="col-md-12">
-        <ul class="cd-gallery">
-
-            <?php for($i=1;$i<=50; $i++){?>
-
+        <?php for($i=1;$i<=50; $i++){?>
+            <ul class="cd-gallery">
+                    <li>
+                        <span class="cd-sale bg-success">New</span>
+                        <a href="javascript:void(0);">
+                            <ul class="cd-item-wrapper">
+                                <li class="selected">
+                                    <img src="<?php echo base_url(); ?>assets/images/shop/ugmonk-tshirt-1.jpg" alt="Preview image">
+                                </li>
+                            </ul>
+                        </a>
+                        <div class="cd-item-info">
+                            <b><a href="#0">Mountains</a></b>
+                            <em class="cd-price">$26</em>
+                        </div>
+                        <div class="cd-item-details">
+                            <a href="javascript:void(0);" class="pull-left add-to-cart" id="1"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                            <a href="#" class="pull-right details"><i class="fa fa-list-ul"></i>Details</a>
+                        </div>
+                    </li>
                 <li>
                     <span class="cd-sale bg-success">New</span>
                     <a href="javascript:void(0);">
@@ -29,9 +45,27 @@
                         <a href="#" class="pull-right details"><i class="fa fa-list-ul"></i>Details</a>
                     </div>
                 </li>
-            <?php } ?>
+                <li>
+                    <span class="cd-sale bg-success">New</span>
+                    <a href="javascript:void(0);">
+                        <ul class="cd-item-wrapper">
+                            <li class="selected">
+                                <img src="<?php echo base_url(); ?>assets/images/shop/ugmonk-tshirt-1.jpg" alt="Preview image">
+                            </li>
+                        </ul>
+                    </a>
+                    <div class="cd-item-info">
+                        <b><a href="#0">Mountains</a></b>
+                        <em class="cd-price">$26</em>
+                    </div>
+                    <div class="cd-item-details">
+                        <a href="javascript:void(0);" class="pull-left add-to-cart" id="1"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                        <a href="#" class="pull-right details"><i class="fa fa-list-ul"></i>Details</a>
+                    </div>
+                </li>
 
-        </ul>
+            </ul>
+        <?php } ?>
     </div>
 </div><!-- Row -->
 
